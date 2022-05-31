225483881de5211e382969bcfe5e970da13c3129
ing181 <ingcei0123@edu.lerum.se>
2022-05-31 12:51:01 +0200

    TUT 4

--- tut1/tut1.ino
+++ tut1/tut1.ino
@@ -1,7 +1,7 @@
 
 int sensorPin = A0;
 int tonePin = 8;
-int sensor;
+int sensor;  // Tar emot ljuset
 void setup() {
   // put your setup code here, to run once:
   pinMode(tonePin,OUTPUT);
@@ -15,10 +15,14 @@ void loop() {
   // put your main code here, to run repeatedly:
   // En inbygd funktio i Arduino
   // "tone" Får Buzzern åt låta med en viss frekvens
-  tone(tonePin,440);
-  delay(1000);
-  // "noTone" stänger av tutet
-  noTone(tonePin);
-  delay(1000);
+  if (sensor < 700){
+
+      tone(tonePin,440);
+  }
+  else {
+      tone(tonePin,200);
+  }
+  
+
 
 }
